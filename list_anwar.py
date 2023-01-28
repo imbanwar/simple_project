6251f751045f26dcd2c86c9d857ecf21ccd134c5
unknown <imbesatanwar90@gmail.com>
2023-01-28 13:15:43 +0530

    file added

---
+++ list_anwar.py
@@ -0,0 +1,17 @@
+import 
+list1= [2,3,5,67,8,9,70]
+list1.reverse()
+print("The reverse lsit is",list1)
+
+list2= [2,3,5,67,8,9,70,65,3,90,73]
+list2.sort()
+print("The sorted list is",list2)
+
+
+list3 =[]
+list3 = list1.copy()
+print("list3=",list3)
+
+
+Indexvalue = list3[2:6]
+print("Indexvalue",indexvalue)
